303c28ac771e4f46101fd7fc5c6b3155675d0a7f
TheExclusiveHub <fsamusev11@gmail.com>
2025-10-16 02:47:24 +0700

    1
    1

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKaKlDD4f22zLRzKu-JfsafBnxwcfvVFA",
+  authDomain: "pvb-gifts.firebaseapp.com",
+  projectId: "pvb-gifts",
+  storageBucket: "pvb-gifts.firebasestorage.app",
+  messagingSenderId: "1025791484911",
+  appId: "1:1025791484911:web:a818cd948d8c6dc2054c1a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
